e650d77527b5bd5404034983c3c2fc9d85ea97fb
Adam Helinski <helinski.adam@outlook.com>
2018-03-05 14:25:23 +0100

    Add user namespace
    This namespace requires all useful namespaces for repl development.

---
+++ dev/user.clj
@@ -0,0 +1,26 @@
+(ns user
+
+  "For daydreaming in the repl."
+
+  (:require [clojure.spec.alpha              :as s]
+            [clojure.spec.gen.alpha          :as gen]
+            [clojure.spec.test.alpha         :as st]
+            [clojure.test.check.clojure-test :as tt]
+            [clojure.test.check.generators   :as tgen]
+            [clojure.test.check.properties   :as tprop]
+            [criterium.core                  :as ct]
+            [dvlopt.i2c                      :as i2c]
+            [dvlopt.i2c.bme280               :as bme280]))
+
+
+
+
+;;;;;;;;;;
+
+
+(comment
+
+  
+
+
+  )
